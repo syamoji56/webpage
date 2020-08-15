e140e4235a4e3fd066d1a1222dce9cdb7224854e
syamoji <36583417+syamoji56@users.noreply.github.com>
2020-08-16 00:38:49 +0900

    feature:数字が入れば3倍にして返す それ以外はそのまま返す

--- chap5.js
+++ chap5.js
@@ -1,10 +1,13 @@
-let ipt = document.querySelector('input');
-let btn = document.querySelector('button');
-let elem = document.querySelector('p');
+let ipt = document.querySelector("input");
+let btn = document.querySelector("button");
+let elem = document.querySelector("p");
 
-
-btn.addEventListener('click',() =>{
+btn.addEventListener("click", () => {
+  if (isNaN(ipt.value)) {
     elem.innerText = ipt.value;
+  } else {
+    elem.innerText = ipt.value * 3;
+  }
 });
 
 // let ipt = document.querySelector('input');
